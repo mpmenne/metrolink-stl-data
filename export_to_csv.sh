66d82a82c35f5544cf64240c5e358326741b0ead
Rebecca Shih <rshih7@gmail.com>
2015-07-27 01:26:39 -0500

    adding lat, long, and slug to the station list

--- export_to_csv.sh
+++ export_to_csv.sh
@@ -7,14 +7,14 @@ sqlite3 metrolink.db < output_stations.sql
 echo "Database generated.  Beginning output to CSV"
 
 IFS="," 
-while read col1 col2
+while read col0 col1 col2 col3 col4 col5 col6
 do
-        no_quotes=${col2//\"}
+        no_quotes=${col6//\"}
         no_ampersands=${no_quotes//&/and}
         url_friendly=${no_ampersands// /_}
         echo "$url_friendly"
         WEEKDAY_FILE_NAME="gen/$url_friendly-mf.csv"
         WEEKEND_FILE_NAME="gen/$url_friendly-ss.csv"
-        sqlite3 -csv  metrolink.db <<< "select * from metrolink_stops where stop_name=$col1 and service_id='1_merged_2038871';" | tee "$WEEKDAY_FILE_NAME" 
-        sqlite3 -csv  metrolink.db <<< "select * from metrolink_stops where stop_name=$col1 and service_id='2_merged_2038873';" | tee "$WEEKEND_FILE_NAME" 
+        sqlite3 -csv  metrolink.db <<< "select * from metrolink_stops where stop_name=$col5 and service_id='1_merged_2038871';" | tee "$WEEKDAY_FILE_NAME" 
+        sqlite3 -csv  metrolink.db <<< "select * from metrolink_stops where stop_name=$col5 and service_id='2_merged_2038873';" | tee "$WEEKEND_FILE_NAME" 
 done < gen/stations.csv
--- output_stations.sql
+++ output_stations.sql
@@ -1,4 +1,4 @@
 .separator ,
 .mode csv
 .output gen/stations.csv
-select stop_name, lower(stop_name) from stops where stop_name like '%METROLINK STATION%' group by stop_name; 
+select stop_lat, stop_id, stop_lon, stop_id, stop_desc, stop_name, replace(replace(lower(stop_name), " ", "_"), "&", "and") from stops where stop_name like '%METROLINK STATION%' group by stop_name; 
