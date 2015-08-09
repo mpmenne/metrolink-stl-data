93dbce260bfbeb8795cc19461f64de1037d3d7b2
Rebecca Shih <rshih7@gmail.com>
2015-08-09 02:44:22 -0500

    lets wipe the existing database when you attepmt to generate a new database

--- generate_database.sh
+++ generate_database.sh
@@ -1,3 +1,7 @@
+if [ -a "metrolink.db" ]; then
+  echo "wiping the existing database"
+  rm metrolink.db
+fi
 touch metrolink.db
 sqlite3 metrolink.db < metrolink.sql
 sqlite3 metrolink.db < import_raw.sql
